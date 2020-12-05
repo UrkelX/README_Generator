84e3b344ab5d01bd0257ac76486a78901fab39e0
JordonMoses <jordon@blackbirdrevolt.com>
2020-12-04 20:21:47 -0600

    Added async function

--- index.js
+++ index.js
@@ -1,69 +1,86 @@
 // Packages
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Prompts
-inquirer 
-    .prompt([
-        {
-            type: 'input',
-            message: "What is your GitHub username? (No @ needed)",
-            name: 'username',
-            default: 'UrkelX',
-        },
-        {
-            type: "input",
-            message: "Project Title",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "Enter Project Description",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "Enter Installation Instructions",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "Enter Usage Instructions",
-            name: "usage"
-        },
-        {
-            type: "input",
-            message: "Enter Contribution Guidelines",
-            name: "contributions"
-        },
-        {
-            type: "input",
-            message: "Provide Test Info and How to Run",
-            name: "tests"
-        },
-        {
-            type: "list",
-            message: "Choose a License for Your Project",
-            choices: ['Apache License 2.0', 'MIT License', 'The Unlicense'],
-            name: "license"
-        },
-        {
-            type: "input",
-            message: "Enter Your Email",
-            name: "Email"
-        }
-
-    ])
-    .then(response => {
-
-        fs.writeFile('README.md', response, err => {
-            if (err) {
-                return console.log(err);
+const readmeQuestions = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "What is your GitHub username? (No @ needed)",
+                name: 'username',
+                default: 'UrkelX',
+            },
+            {
+                type: "input",
+                message: "Project Title",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Enter Project Description",
+                name: "description"
+            },
+            {
+                type: "input",
+                message: "Enter Installation Instructions",
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: "Enter Usage Instructions",
+                name: "usage"
+            },
+            {
+                type: "input",
+                message: "Enter Contribution Guidelines",
+                name: "contributions"
+            },
+            {
+                type: "input",
+                message: "Provide Test Info and How to Run",
+                name: "tests"
+            },
+            {
+                type: "list",
+                message: "Choose a License for Your Project",
+                choices: ['Apache License 2.0', 'MIT License', 'The Unlicense'],
+                name: "license"
+            },
+            {
+                type: "input",
+                message: "Enter Your Email",
+                name: "Email"
             }
-            
-            console.log("Success! Your README has been created")
-        });
 
-    })
+        ]);
+};
 
-   
+const generateContent = (answers) =>
+    `
+    ## ${answers.title}
+    
+    
+    `;
+
+const init = async () => {
+    console.log('hello');
+    try {
+        const answers = await readmeQuestions();
+
+        const readmeContent = generateContent(answers);
+
+        await writeFileAsync('REAME.md', readmeContent);
+
+        console.log('Successfully wrote to README.md');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+init();
+
